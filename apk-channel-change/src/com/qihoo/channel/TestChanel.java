5624ec7fc3b24351e24f74cd392bfa7804c553ac
anxianjie <anxianjie362744930@gmail.com>
2017-04-06 17:18:59 +0800

    project name update

--- apk-channel-change/src/com/qihoo/channel/TestChanel.java
+++ apk-channel-change/src/com/qihoo/channel/TestChanel.java
@@ -14,7 +14,7 @@ public class TestChanel {
 		file = new File(
 				"C:\\Users\\anxianjie-g\\Desktop\\149623_360Gameunion-CI_signed-1489718193\\GameUnion-CI-2017-03-17_10-30-55_signed_aligned.apk");
 		// loadChannel = Reader.loadChannel(file);
-		 Writer.saveChannel(file, "gu_123456789");//gu_123456789：需要更改的渠道号
+//		 Writer.saveChannel(file, "gu_123456789");//gu_123456789：需要更改的渠道号
 		loadChannel = Reader.loadChannel(file);
 		System.out.println("loadChannel=" + loadChannel);
 
